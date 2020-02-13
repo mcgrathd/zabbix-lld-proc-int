67e3eb7f6436a21aaaec5594f4d4632ceaebc2b7
Danny McGrath <danmcgrath.ca@gmail.com>
2020-02-13 15:53:14 -0500

    Wrap code in sub

--- proc_interrupts.pl
+++ proc_interrupts.pl
@@ -6,21 +6,24 @@ use warnings;
 use strict;
 use constant DEBUG => 0;
 my $first = 1;
- 
-print "{\n";
-print "\t\"data\":[\n\n";
- 
-for (`/bin/cat /proc/diskstats | /usr/bin/awk '{ print \$3 }'`)
-{
-    (my $device) = m/(\S+)/;
 
-    print "\t,\n" if not $first;
-    $first = 0;
- 
-    print "\t{\n";
-    print "\t\t\"{#DEVICE}\":\"$device\"\n";
-    print "\t}\n";
+sub display {
+    print "{\n";
+    print "\t\"data\":[\n\n";
+
+    for (`/bin/cat /proc/diskstats | /usr/bin/awk '{ print \$3 }'`)
+    {
+        (my $device) = m/(\S+)/;
+
+        print "\t,\n" if not $first;
+        $first = 0;
+
+        print "\t{\n";
+        print "\t\t\"{#DEVICE}\":\"$device\"\n";
+        print "\t}\n";
+    }
+
+    print "\n\t]\n";
+    print "}\n";
 }
- 
-print "\n\t]\n";
-print "}\n";
+
